f7259534af2bfcf3487e2b3fe5d36e5277def185
bmudhapati <bmudhapati957@millardps.org>
2018-07-26 20:31:57 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -4,7 +4,7 @@
 
 def recipe_ingredients(perfect_10_recipe)
 perfect_10_recipe = {}
-perfect_10_recipe = {:almond_flour => "2 cups", :gluten_freewhole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder => "1/2 tea spoon", :baking_soda => "1/4 tea spoon", :Xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
+perfect_10_recipe = {:almond_flour => "2 cups", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder => "1/2 tea spoon", :baking_soda => "1/4 tea spoon", :xanthan_Gum => "1/2 tea spoon", :slivered_almonds => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
 end
 
 #  - almond flour - 2 cups
